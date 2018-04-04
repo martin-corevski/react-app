ffdb6fc179ad2954013deed85ed09a1948be42dd
Martin Corevski <martin-corevski@outlook.com>
2018-04-04 15:16:23 +0200

    Axios added
    Http requests library installed
    Readme updates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom'
 import { Route, HashRouter } from 'react-router-dom'
 import './index.scss'
 import App from './containers/App'
+import axios from 'axios'
+
+// Set up axios default configuration
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
+
+// https://github.com/axios/axios#interceptors
+axios.interceptors.request.use(request => {
+  console.log('axios interceptors request: ', request)
+  // Edit request config
+  return request
+}, error => {
+  // request error handling, like loss of internet connection, server problems..
+  console.log('axios interceptors error: ', error)
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+  console.log('axios interceptors response: ', response)
+  // Edit response config
+  return response
+}, error => {
+  // request error handling, like loss of internet connection, server problems..
+  console.log('axios interceptors error: ', error)
+  return Promise.reject(error)
+})
 
 ReactDOM.render(
   <HashRouter>
