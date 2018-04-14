406e4dd432a83f1a08f1023dea0e7f292342c696
Martin Corevski <martin-corevski@outlook.com>
2018-04-14 21:31:27 +0200

    Testing basics implemented

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  setupFiles: ['<rootDir>/config/test-setup.js'],
+  setupFiles: ['<rootDir>/config/tests-setup.js'],
+  transform: { '^.+\\.(jsx|js)?$': '<rootDir>/node_modules/babel-jest' },
   moduleNameMapper: { '^.+\\.(css|scss)$': 'identity-obj-proxy' }
 }
